569d6c42222b360dc03d602e9070e1e86b7fda48
illia <ulyanov.iv@gmail.com>
2024-10-14 11:05:03 +0200

    use chat history compression to improve content retrieval quality

--- src/main/java/com/capgemini/demos/rag/configuration/AssistantConfiguration.java
+++ src/main/java/com/capgemini/demos/rag/configuration/AssistantConfiguration.java
@@ -3,8 +3,13 @@ package com.capgemini.demos.rag.configuration;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.bgesmallenv15q.BgeSmallEnV15QuantizedEmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.query.transformer.CompressingQueryTransformer;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.opensearch.OpenSearchEmbeddingStore;
 import org.springframework.context.annotation.Bean;
@@ -29,5 +34,13 @@ public class AssistantConfiguration {
     @Bean
     EmbeddingModel embeddingModel() {
         return new BgeSmallEnV15QuantizedEmbeddingModel();
+    }
+
+    @Bean
+    RetrievalAugmentor retrievalAugmentor(ChatLanguageModel model, ContentRetriever contentRetriever) {
+        return DefaultRetrievalAugmentor.builder()
+            .contentRetriever(contentRetriever)
+            .queryTransformer(new CompressingQueryTransformer(model))
+            .build();
     }
 }
