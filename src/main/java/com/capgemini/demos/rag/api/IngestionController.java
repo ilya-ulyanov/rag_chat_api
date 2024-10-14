49c620447f8bc306d4b385aa923044649aece694
illia <ulyanov.iv@gmail.com>
2024-10-14 17:11:18 +0200

    tweak document splitter to produce bigger chunks

--- src/main/java/com/capgemini/demos/rag/api/IngestionController.java
+++ src/main/java/com/capgemini/demos/rag/api/IngestionController.java
@@ -3,7 +3,10 @@ package com.capgemini.demos.rag.api;
 import com.capgemini.demos.rag.configuration.IngestionProperties;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.HuggingFaceTokenizer;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +21,19 @@ import java.util.List;
 public class IngestionController {
     private final IngestionProperties properties;
     private final EmbeddingStore<TextSegment> embeddingStore;
+    private final EmbeddingModel embeddingModel;
 
     @PostMapping("sync_docs")
     public ResponseEntity<Boolean> syncDocuments() {
         List<Document> documents = FileSystemDocumentLoader.loadDocumentsRecursively(properties.getDocumentsPath());
-        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+        var ingestor = EmbeddingStoreIngestor
+            .builder()
+            .embeddingStore(embeddingStore)
+            .embeddingModel(embeddingModel)
+            .documentSplitter(DocumentSplitters.recursive(1000, 200, new HuggingFaceTokenizer()))
+            .build();
+
+        ingestor.ingest(documents);
         return ResponseEntity.ok(true);
     }
 }
